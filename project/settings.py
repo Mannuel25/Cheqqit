c3dba16497ac521b707166300ab730c378ec99e2
mannuel25 <oluwasegunprosperity@gmail.com>
2021-09-28 19:41:55 +0100

    add

--- project/settings.py
+++ project/settings.py
@@ -129,3 +129,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
