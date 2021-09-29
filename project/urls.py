6eea5a443497e30da417d06fae6a2f0de6f46537
mannuel25 <oluwasegunprosperity@gmail.com>
2021-09-29 17:21:12 +0100

    push

--- project/urls.py
+++ project/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', include('cheqqit.urls')),
+    path('cheqqit/', include('cheqqit.urls')),
+    path('cheqqit/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'),
+        name='home'),
 ]
