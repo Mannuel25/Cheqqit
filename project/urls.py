04cc46dd017241d259518c089d20b045051eace0
mannuel25 <oluwasegunprosperity@gmail.com>
2021-09-29 17:30:16 +0100

    add

--- project/urls.py
+++ project/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cheqqit/', include('cheqqit.urls')),
     path('cheqqit/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'),
-        name='home'),
+    path('', include('pages.urls')),
 ]
