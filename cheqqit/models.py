d461b7e0a93e20363c58c7a418ff17b99c5e3700
mannuel25 <oluwasegunprosperity@gmail.com>
2021-10-13 12:58:27 +0100

    initial commit

--- cheqqit/models.py
+++ cheqqit/models.py
@@ -2,4 +2,4 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-  email = models.EmailField(max_length=254)
+  pass
--- cheqqit/views.py
+++ cheqqit/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
