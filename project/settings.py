afce8b7cb5d21bf3c125149cf4ca023c682c7190
mannuel25 <oluwasegunprosperity@gmail.com>
2021-10-21 18:09:06 +0100

    configured crispy forms

--- project/settings.py
+++ project/settings.py
@@ -41,7 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cheqqit',
     'pages',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
