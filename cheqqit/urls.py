02e687cc73a23ca0cd7ec48d21bcbf5f42904c32
mannuel25 <oluwasegunprosperity@gmail.com>
2021-10-22 22:22:16 +0100

    updated urlpatterns

--- cheqqit/urls.py
+++ cheqqit/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import SignUpView
+from . import views
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
+    path('signup', views.signupPage, name='signup'),
+    path('signin/', views.signinPage, name='login'),
+    path('signout/', views.signoutUser, name='logout'),
 ]
