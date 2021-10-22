c4354605a54ffb0397a7738d39596d313db87605
mannuel25 <oluwasegunprosperity@gmail.com>
2021-10-22 22:21:16 +0100

    fixed sign in and sign out views

--- cheqqit/views.py
+++ cheqqit/views.py
@@ -1,8 +1,41 @@
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import *
 from .forms import CustomUserCreationForm
 
-class SignUpView(CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+def signupPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+        if request.method == 'POST':
+            form = CustomUserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('login')
+        context = {'form': form}
+        return render(request, 'signup.html', context)
+
+def signinPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, "Invalid username or password")
+        context = {}
+        return render(request, 'registration/login.html', context)
+
+def signoutUser(request):
+    logout(request)
+    return redirect('home')
