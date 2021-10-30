08883231a3fb397b232657a2284cd7877c5c93f5
mannuel25 <oluwasegunprosperity@gmail.com>
2021-10-30 23:43:44 +0100

    add

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import HomePageView
 from .views import FeaturesPageView
-
+from .views import WebappPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('features/', FeaturesPageView.as_view(), name='features'),
+    path('webapp/', WebappPageView.as_view(), name='webapp'),
 ]
