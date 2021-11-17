10b8510df50355d844f86a63606acc838fb57529
Emmanuel Tanimowo <Oluwasegunprosperity@gmail.com>
2021-11-17 19:30:08 +0000

    update

--- cheqqit/views.py
+++ cheqqit/views.py
@@ -5,29 +5,42 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from .models import Tasks
 
 def signupPage(request):
-    form = CustomUserCreationForm()
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-    context = {'form': form}
-    return render(request, 'signup.html', context)
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+        if request.method == 'POST':
+            form = CustomUserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('login')
+        context = {'form': form}
+        return render(request, 'signup.html', context)
 
 def signinPage(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('webapp')
-        else:
-            messages.error(request, "Invalid username or password")
-    context = {}
-    return render(request, 'registration/login.html', context)
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('webapp')
+            else:
+                messages.error(request, "Invalid username or password")
+        context = {}
+        return render(request, 'registration/login.html', context)
 
 def signoutUser(request):
     logout(request)
