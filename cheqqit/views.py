e5cee05f215402c60d20c22c754db9c520451d67
Emmanuel Tanimowo <Oluwasegunprosperity@gmail.com>
2021-11-17 20:50:06 +0000

    Update views.py

--- cheqqit/views.py
+++ cheqqit/views.py
@@ -3,15 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
-from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.decorators import login_required
-from .models import Tasks
 
 def signupPage(request):
     if request.user.is_authenticated:
