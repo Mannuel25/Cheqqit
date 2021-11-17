fca3e29f61a11d6f259da8b706e84110f1000d87
Emmanuel Tanimowo <Oluwasegunprosperity@gmail.com>
2021-11-17 20:52:21 +0000

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,11 @@
 from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from .models import Tasks
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
@@ -6,5 +13,12 @@ class HomePageView(TemplateView):
 class FeaturesPageView(TemplateView):
     template_name = 'features.html'
 
-class WebappPageView(TemplateView):
+class WebappPageView(ListView):
     template_name = 'webapp.html'
+
+class TasksList(LoginRequiredMixin, ListView):
+    model = Tasks
+    context_object_name = 'tasks'
+
+    def get_context_data(self, **kwargs):
+        
