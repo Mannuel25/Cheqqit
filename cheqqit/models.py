dce7b182f0c8fd5e1b6ec09798cd3a365a5814af
Emmanuel Tanimowo <Oluwasegunprosperity@gmail.com>
2021-11-18 11:11:06 +0000

    Update models.py

--- cheqqit/models.py
+++ cheqqit/models.py
@@ -1,23 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth import get_user_model
 
 class CustomUser(AbstractUser):
     username = models.CharField(max_length=20,unique=True)
     email = models.EmailField(max_length=70,unique=True)
     password = models.CharField(max_length=12)  
-
-class Tasks(models.Model):
-    user = models.ForeignKey(
-        get_user_model(), on_delete=models.CASCADE, null=True, blank=True)
-    title = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
-    completed_task = models.BooleanField(default=False)
-    task_created_when = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        order_with_respect_to = 'user'
-
