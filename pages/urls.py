8dae01f2a7769dc0a9d196deb1a1f7fb398e2d5f
Emmanuel Tanimowo <Oluwasegunprosperity@gmail.com>
2021-11-18 14:28:51 +0000

    Update urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import HomePageView
 from .views import FeaturesPageView
 from .views import WebappPageView
+from .views import InboxView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('features/', FeaturesPageView.as_view(), name='features'),
     path('webapp/', WebappPageView.as_view(), name='webapp'),
+    path('inbox/', InboxView.as_view(), name='inbox'),
 ]
