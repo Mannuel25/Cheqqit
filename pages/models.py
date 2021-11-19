c60b66a39ce1524130ed2155ea3482dbd52e9412
Emmanuel Tanimowo <Oluwasegunprosperity@gmail.com>
2021-11-19 21:18:32 +0100

    Update models.py

--- pages/models.py
+++ pages/models.py
@@ -5,9 +5,9 @@ from django.contrib.auth import get_user_model
 class UserTasks(models.Model):
     user = models.ForeignKey(
         get_user_model(), on_delete=models.CASCADE, null=True, blank=True)
-    title = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
-    completed_task = models.BooleanField(default=False)
+    task_title = models.CharField(max_length=200)
+    task_description = models.TextField(null=True, blank=True)
+    completed_tasks = models.BooleanField(default=False)
     task_created_when = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
