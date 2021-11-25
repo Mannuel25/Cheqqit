c6811acb13fbbc2edab9f4675bc78cbb3f28f77e
Emmanuel Tanimowo <Oluwasegunprosperity@gmail.com>
2021-11-25 12:17:36 +0000

    fixx

--- cheqqit/views.py
+++ cheqqit/views.py
@@ -1,39 +1,38 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
-from .forms import CustomUserCreationForm
+from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import UserTasks
 
-def signupPage(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    else:
-        form = CustomUserCreationForm()
-        if request.method == 'POST':
-            form = CustomUserCreationForm(request.POST)
-            if form.is_valid():
-                form.save()
-                return redirect('login')
-        context = {'form': form}
-        return render(request, 'signup.html', context)
+class HomePageView(TemplateView):
+    template_name = 'home.html'
 
-def signinPage(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    else:
-        if request.method == 'POST':
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('inbox')
-            else:
-                messages.error(request, "Invalid username or password")
-        context = {}
-        return render(request, 'registration/login.html', context)
+class FeaturesPageView(TemplateView):
+    template_name = 'features.html'
 
-def signoutUser(request):
-    logout(request)
-    return redirect('home')
+class WebappPageView(TemplateView):
+    template_name = 'webapp.html'
+
+class InboxView(LoginRequiredMixin, ListView):
+    model = UserTasks
+    context_object_name = 'tasks'
+    template_name = 'inbox.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print('Context_1:', context)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(completed_task=False).count()
+        print('Context_2:', context)
+
+        search_input = self.request.GET.get('search-area') or ''
+        print('SEARCH INPUT:', search_input)
+        if search_input:
+            context['tasks'] = context['tasks'].filter(
+                title__icontains=search_input)
+
+        context['search_input'] = search_input
+
+        return context  
