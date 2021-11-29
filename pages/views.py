aaead90ad6c46c5a01d429fb82c1ab9600a7fa10
Emmanuel Tanimowo <Oluwasegunprosperity@gmail.com>
2021-11-29 13:48:08 +0000

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,45 @@
-from django import forms
+from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import UserTasks
+from .forms import TaskDetails
 
-class TaskDetails(forms.ModelForm):
-    title = forms.CharField(max_length=100, widget= forms.TextInput
-        (attrs={'placeholder':'Enter the task/goal title'}))
-    description = forms.CharField(max_length=150, required=False, widget= forms.Textarea
-        (attrs={'placeholder':'Enter the task/goal description'}))
+class HomePageView(TemplateView):
+    template_name = 'home.html'
 
-    class Meta(forms.ModelForm):
-        model = UserTasks
-        fields = ('title', 'description',)
+class FeaturesPageView(TemplateView):
+    template_name = 'features.html'
+
+class WebappPageView(TemplateView):
+    template_name = 'webapp.html'
+
+class InboxView(LoginRequiredMixin, ListView):
+    model = UserTasks
+    context_object_name = 'tasks'
+    template_name = 'inbox.html'
+    login_url = 'login'
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print('Context_1:', context)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(completed_task=False).count()
+        print('Context_2:', context)
+
+        search_input = self.request.GET.get('search-area') or ''
+        print('SEARCH INPUT:', search_input)
+        if search_input:
+            context['tasks'] = context['tasks'].filter(
+                title__icontains=search_input)
+
+        context['search_input'] = search_input
+
+        return context  
+
+class CreateTaskView(LoginRequiredMixin, CreateView):
+    form_class = TaskDetails
+    template_name = 'create_task.html'
+    success_url = reverse_lazy('inbox')
