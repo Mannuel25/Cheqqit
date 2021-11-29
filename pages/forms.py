1fe9b851a81b825010c19fa7b1f552eda99534a9
Emmanuel Tanimowo <Oluwasegunprosperity@gmail.com>
2021-11-29 13:49:08 +0000

    Create forms.py

---
+++ pages/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import UserTasks
+
+class TaskDetails(forms.ModelForm):
+    title = forms.CharField(max_length=100, widget= forms.TextInput
+        (attrs={'placeholder':'Enter the task/goal title'}))
+    description = forms.CharField(max_length=150, required=False, widget= forms.Textarea
+        (attrs={'placeholder':'Enter the task/goal description'}))
+
+    class Meta(forms.ModelForm):
+        model = UserTasks
+        fields = ('title', 'description',)
+    
