dc50fd6cebf8b87bc120921725d63798d3520299
Emmanuel Tanimowo <Oluwasegunprosperity@gmail.com>
2021-12-06 16:56:31 +0000

    Update views.py

--- cheqqit/views.py
+++ cheqqit/views.py
@@ -1,38 +1,8 @@
-from django.views.generic import TemplateView
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
-from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import UserTasks
+from django.urls import path
+from . import views
 
-class HomePageView(TemplateView):
-    template_name = 'home.html'
-
-class FeaturesPageView(TemplateView):
-    template_name = 'features.html'
-
-class WebappPageView(TemplateView):
-    template_name = 'webapp.html'
-
-class InboxView(LoginRequiredMixin, ListView):
-    model = UserTasks
-    context_object_name = 'tasks'
-    template_name = 'inbox.html'
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        print('Context_1:', context)
-        context['tasks'] = context['tasks'].filter(user=self.request.user)
-        context['count'] = context['tasks'].filter(completed_task=False).count()
-        print('Context_2:', context)
-
-        search_input = self.request.GET.get('search-area') or ''
-        print('SEARCH INPUT:', search_input)
-        if search_input:
-            context['tasks'] = context['tasks'].filter(
-                title__icontains=search_input)
-
-        context['search_input'] = search_input
-
-        return context  
+urlpatterns = [
+    path('signup', views.signupPage, name='signup'),
+    path('signin/', views.signinPage, name='login'),
+    path('signout/', views.signoutUser, name='logout'),
+]
