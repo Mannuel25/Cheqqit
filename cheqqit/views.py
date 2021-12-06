6950ea97a50cc5598e8bebe4786cbd914ed9b8ec
Emmanuel Tanimowo <Oluwasegunprosperity@gmail.com>
2021-12-06 16:58:08 +0000

    Update views.py

--- cheqqit/views.py
+++ cheqqit/views.py
@@ -1,8 +1,39 @@
-from django.urls import path
-from . import views
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import CustomUserCreationForm
 
-urlpatterns = [
-    path('signup', views.signupPage, name='signup'),
-    path('signin/', views.signinPage, name='login'),
-    path('signout/', views.signoutUser, name='logout'),
-]
+def signupPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+        if request.method == 'POST':
+            form = CustomUserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('login')
+        context = {'form': form}
+        return render(request, 'signup.html', context)
+
+def signinPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('inbox')
+            else:
+                messages.error(request, "Invalid username or password")
+        context = {}
+        return render(request, 'registration/login.html', context)
+
+def signoutUser(request):
+    logout(request)
+    return redirect('home')
