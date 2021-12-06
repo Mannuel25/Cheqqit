ef8b02636f09997f261a6d7bf12d9b1e4e699f35
Emmanuel Tanimowo <Oluwasegunprosperity@gmail.com>
2021-12-06 18:23:32 +0000

    Update models.py

--- pages/models.py
+++ pages/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import get_user_model
 
 class UserTasks(models.Model):
     user = models.ForeignKey(
         get_user_model(), on_delete=models.CASCADE, null=True, blank=True)
-    task_title = models.CharField(max_length=200)
-    task_description = models.TextField(null=True, blank=True)
-    completed_tasks = models.BooleanField(default=False)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    completed_task = models.BooleanField(default=False)
     task_created_when = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
