ff0c15e4aa408b9662dfb14ea6fea4a8c9530666
Emmanuel Tanimowo <Oluwasegunprosperity@gmail.com>
2021-12-12 13:38:31 +0000

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -21,11 +21,18 @@ class InboxView(LoginRequiredMixin, ListView):
     template_name = 'inbox.html'
     login_url = 'login'
     context_object_name = 'tasks'
-
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        print('CONTEXT DATA 1:', context)
+        all_tasks = [i for i in context['tasks'].filter(user = self.request.user)]
+        print('ALL USER TASKS 2:', all_tasks)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
         context['count'] = context['tasks'].filter(completed_task=False).count()
+        print('CONTEXT 2:', context)
+        all_incomplete = [i for i in context['tasks'].filter(user=self.request.user, completed_task=False)]
+        print('\n\nall incomplete tasks:', all_incomplete)
+        # print(all_incomplete[0])
 
         search_input = self.request.GET.get('search-area') or ''
         if search_input:
@@ -45,4 +52,9 @@ class CreateTaskView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
-    
+
+# class TaskDetailView(LoginRequiredMixin, DetailView):
+#     model = Task
+#     context_object_name = 'task'
+#     template_name = 'task_detail.html'
+#     login_url = 'login'
