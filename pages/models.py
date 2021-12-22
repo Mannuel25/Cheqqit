b61fd45e909b0416984deda62c8c5e215a67388a
mannuel25 <oluwasegunprosperity@gmail.com>
2021-12-22 19:40:12 +0000

    updated task model field

--- pages/models.py
+++ pages/models.py
@@ -7,8 +7,8 @@ class UserTasks(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     completed_task = models.BooleanField(default=False)
-    task_created_when = models.DateTimeField(auto_now_add=True)
-
+    task_created_when = models.DateTimeField()
+    
     def __str__(self):
         return self.title
         
