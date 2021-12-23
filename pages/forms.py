35b82dd5ff442b94f73d043187ffe0ea72d7908e
mannuel25 <oluwasegunprosperity@gmail.com>
2021-12-23 15:05:30 +0000

    adjusted time and date fields

--- pages/forms.py
+++ pages/forms.py
@@ -6,13 +6,13 @@ class TaskDetails(forms.ModelForm):
         (attrs={'placeholder':'Enter the task/goal title'}))
     description = forms.CharField(max_length=150, required=False, widget= forms.Textarea
         (attrs={'placeholder':'Enter the task/goal description'}))
-    task_due_date = forms.DateTimeField(
-        label="Task/goal due date",
-        widget=forms.widgets.DateTimeInput(attrs={'type':'date'})
+    task_due_date = forms.DateField(
+        label="Task/goal due date", required=False,
+        widget=forms.widgets.DateInput(attrs={'type':'date'})
     )
-    task_due_time = forms.DateTimeField(
-        label="Task/goal due time",
-        widget=forms.widgets.DateTimeInput(attrs={'type':'time'})
+    task_due_time = forms.TimeField(
+        label="Task/goal due time", required=False,
+        widget=forms.widgets.TimeInput(attrs={'type':'time'})
     )
 
     class Meta(forms.ModelForm):
