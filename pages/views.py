9c7f1e80bdc20bdfb83b8d26ad256ce4db198c87
mannuel25 <oluwasegunprosperity@gmail.com>
2021-12-26 21:53:58 +0000

    update..

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormMixin
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import UserTasks
@@ -46,42 +46,22 @@ class InboxView(LoginRequiredMixin, ListView):
 
         return context
 
-class CreateTaskView(LoginRequiredMixin, CreateView, ListView):
+class CreateTaskView(LoginRequiredMixin, CreateView):
     form_class = TaskDetails
     template_name = 'create_task.html'
     success_url = reverse_lazy('inbox')
     login_url = 'login'
-    model = UserTasks
-    context_object_name = 'tasks'
+   
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
-    
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        print('CONTEXT DATA 1:', context)
-        all_tasks = [i for i in context['tasks'].filter(user = self.request.user)]
-        print('ALL USER TASKS 2:', all_tasks)
-        context['tasks'] = context['tasks'].filter(user=self.request.user)
-        context['count'] = context['tasks'].filter(completed_task=False).count()
-        print('CONTEXT 2:', context)
-        all_incomplete = [i for i in context['tasks'].filter(user=self.request.user, completed_task=False)]
-        print('\n\nall incomplete tasks:', all_incomplete)
-        # print(all_incomplete[0])
 
-        search_input = self.request.GET.get('search-area') or ''
-        if search_input:
-            context['tasks'] = context['tasks'].filter(
-                title__contains=search_input)
-
-        context['search_input'] = search_input
-
-        return context
 
 # class TaskDetailView(LoginRequiredMixin, DetailView):
 #     model = Task
 #     context_object_name = 'task'
 #     template_name = 'task_detail.html'
 #     login_url = 'login'
+
+
