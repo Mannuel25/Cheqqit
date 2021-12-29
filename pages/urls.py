e81f69e7ea8a94df7126f05429bf0672c7e20aca
mannuel25 <oluwasegunprosperity@gmail.com>
2021-12-29 02:12:24 +0000

    added task detail url

--- pages/urls.py
+++ pages/urls.py
@@ -3,7 +3,7 @@ from .views import HomePageView
 from .views import FeaturesPageView
 from .views import WebappPageView
 from .views import InboxView
-from .views import CreateTaskView, UpdateTask
+from .views import CreateTaskView, UpdateTask, TaskDetail
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
@@ -12,4 +12,5 @@ urlpatterns = [
     path('inbox/', InboxView.as_view(), name='inbox'),
     path('create_task/', CreateTaskView.as_view(), name='create_task'),
     path('update_task/<slug:slug>/', UpdateTask, name='update_task'),
+    path('task_detail/<slug:slug>/', TaskDetail, name='task_detail'),
 ]
