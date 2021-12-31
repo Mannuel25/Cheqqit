3f1dacb42e1d1372d3c09b4e39c5aa1486b22af1
mannuel25 <oluwasegunprosperity@gmail.com>
2021-12-31 18:02:40 +0000

    updated field labels

--- pages/forms.py
+++ pages/forms.py
@@ -7,15 +7,15 @@ class TaskDetailsForm(forms.ModelForm):
     or update the task/goal created
     """
     title = forms.CharField(max_length=100, widget= forms.TextInput
-        (attrs={'placeholder':"Enter task/goal's title"}))
+        (attrs={'placeholder':"Enter the task's title"}))
     description = forms.CharField(max_length=150, required=False, widget= forms.Textarea
-        (attrs={'placeholder':"Enter task/goal's description"}))
+        (attrs={'placeholder':"Enter the task's description"}))
     task_due_date = forms.DateField(
-        label="Select task/goal's due date", required=False,
+        label="Select the task's due date", required=False,
         widget=forms.widgets.DateInput(attrs={'type':'date'})
     )
     task_due_time = forms.TimeField(
-        label="Select task/goal's due time", required=False,
+        label="Select the task's due time", required=False,
         widget=forms.widgets.TimeInput(attrs={'type':'time'})
     )
 
@@ -27,16 +27,16 @@ class ViewTaskDetailsForm(forms.ModelForm):
     """
     this form will be used to only view the task/goal created
     """
-    title = forms.CharField(max_length=100, label="Task/goal's title",
+    title = forms.CharField(max_length=100, label="Task's title",
         required=False, widget= forms.TextInput(attrs={'readonly':'readonly'}))
-    description = forms.CharField(label="Task/goal's description", 
+    description = forms.CharField(label="Task's description", 
         required=False, widget= forms.Textarea(attrs={'readonly':'readonly'}))
     task_due_date = forms.DateField(
-        label="Task/goal's due date", required=False,
+        label="Task's due date", required=False,
         widget=forms.widgets.DateInput(attrs={'type':'date','readonly':'readonly'})
     )
     task_due_time = forms.TimeField(
-        label="Task/goal's due time", required=False,
+        label="Task's due time", required=False,
         widget=forms.widgets.TimeInput(attrs={'type':'time','readonly':'readonly'})
     )
 
