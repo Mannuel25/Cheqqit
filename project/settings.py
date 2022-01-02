5784e85ef5da33575ff297eb09dc674fe8b4c81f
mannuel25 <oluwasegunprosperity@gmail.com>
2022-01-02 16:11:11 +0000

    update

--- project/settings.py
+++ project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 AUTH_USER_MODEL = 'cheqqit.CustomUser'
 
