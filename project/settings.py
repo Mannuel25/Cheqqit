b54c73d797119fce35c5573d9a2a64f1e3b067a9
mannuel25 <oluwasegunprosperity@gmail.com>
2022-01-16 20:02:29 +0000

    add

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
