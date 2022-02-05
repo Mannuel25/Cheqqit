26f8e0f2bb01f81fdadea4a4baa0e859cb7c1c6d
mannuel25 <oluwasegunprosperity@gmail.com>
2022-02-05 00:26:56 +0000

    fix

--- cheqqit/models.py
+++ cheqqit/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class CustomUser(AbstractUser):
     username = models.CharField(max_length=20,unique=True)
     email = models.EmailField(max_length=70,unique=True)
-    password = models.CharField(max_length=20)  
+    # password = models.CharField()  
