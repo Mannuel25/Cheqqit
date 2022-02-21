4a64a43f06d650097e2beefce1214232138d6831
mannuel25 <oluwasegunprosperity@gmail.com>
2022-02-21 20:53:54 +0000

    fixed typo in url

--- cheqqit/urls.py
+++ cheqqit/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup', views.signupPage, name='signup'),
+    path('signup/', views.signupPage, name='signup'),
     path('signin/', views.signinPage, name='login'),
     path('signout/', views.signoutUser, name='logout'),
 ]
