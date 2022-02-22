e06c96d4f524b144366bc1b69f2247b5364c3b08
mannuel25 <oluwasegunprosperity@gmail.com>
2022-02-22 22:16:18 +0000

    edit ++

--- pages/urls.py
+++ pages/urls.py
@@ -3,7 +3,7 @@ from .views import HomePageView
 from .views import FeaturesPageView
 from .views import WebappPageView
 from .views import InboxView
-from .views import CreateTaskView, UpdateTask, TaskDetail, DeleteTask
+from .views import CreateTaskView, UpdateTaskView, TaskDetail, DeleteTask
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
@@ -11,7 +11,7 @@ urlpatterns = [
     path('webapp/', WebappPageView.as_view(), name='webapp'),
     path('inbox/', InboxView.as_view(), name='inbox'),
     path('create_task/', CreateTaskView.as_view(), name='create_task'),
-    path('update_task/<slug:slug>/', UpdateTask, name='update_task'),
+    path('update_task/<slug:slug>/', UpdateTaskView.as_view(), name='update_task'),
     path('task_detail/<slug:slug>/', TaskDetail, name='task_detail'),
     path('delete_task/<slug:slug>/', DeleteTask, name='delete_task'),
 ]
