681962ae98ec44a3bc46c076b0cc1c0fe798cfc9
mannuel25 <oluwasegunprosperity@gmail.com>
2022-02-25 22:19:49 +0000

    add ++

--- pages/urls.py
+++ pages/urls.py
@@ -3,7 +3,7 @@ from .views import HomePageView
 from .views import FeaturesPageView
 from .views import WebappPageView
 from .views import InboxView
-from .views import CreateTaskView, UpdateTask, TaskDetail, DeleteTask
+from .views import CreateTaskView, UpdateTask, TaskDetail, DeleteTask,TodayView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
@@ -14,6 +14,7 @@ urlpatterns = [
     path('update_task/<slug:slug>/', UpdateTask, name='update_task'),
     path('task_detail/<slug:slug>/', TaskDetail, name='task_detail'),
     path('delete_task/<slug:slug>/', DeleteTask, name='delete_task'),
+    path('today/', TodayView.as_view(), name='today'),
 ]
 
 handler404 = 'pages.views.page_not_found'
