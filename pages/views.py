6fc10083a9e727df19c07ee0b06bdbe2165e89bd
mannuel25 <oluwasegunprosperity@gmail.com>
2022-03-06 16:25:05 +0100

    fixx

--- pages/views.py
+++ pages/views.py
@@ -91,8 +91,9 @@ class TodayView(LoginRequiredMixin, CreateView, ListView):
     model = UserTasks
     form_class = AllTasksForm
     template_name = 'today.html'
-    success_url = reverse_lazy('inbox') 
+    success_url = reverse_lazy('today') 
     context_object_name = 'tasks'
+    login_url = 'login'
     format_today_date = datetime.today().strftime('%Y-%m-%d')
 
     def form_valid(self, form):
@@ -139,9 +140,9 @@ class CreateTaskView(LoginRequiredMixin, CreateView, ListView):
     model = UserTasks
     form_class = TaskDetailsForm
     template_name = 'create_task.html'
-    success_url = reverse_lazy('inbox') 
     context_object_name = 'tasks'
     success_url = reverse_lazy('inbox')
+    login_url = 'login'
 
     def form_valid(self, form):
         form.instance.user = self.request.user
@@ -172,8 +173,9 @@ class CompletedTasksView(LoginRequiredMixin, CreateView, ListView):
     model = UserTasks
     form_class = AllTasksForm
     template_name = 'completed_tasks.html'
-    success_url = reverse_lazy('inbox') 
+    success_url = reverse_lazy('completed_tasks') 
     context_object_name = 'tasks'
+    login_url = 'login'
 
     def form_valid(self, form):
         if self.request.method == 'POST':
@@ -206,16 +208,17 @@ class CompletedTasksView(LoginRequiredMixin, CreateView, ListView):
         context['search_input'] = search_input
         return context
 
+@login_required(login_url='login')
 def UpdateTask(request, slug):
-        user_task = get_object_or_404(UserTasks, slug=slug)
-        form = TaskDetailsForm(instance=user_task)
-        if request.method == 'POST':
-            form = TaskDetailsForm(request.POST, instance=user_task)
-            if form.is_valid():
-                form.save()
-                return redirect('inbox')
-        context = {'form':form, 'slug':slug, 'no_of_undone_tasks':lst_undone_task[-1]}
-        return render(request, 'update_task.html', context)
+    user_task = get_object_or_404(UserTasks, slug=slug)
+    form = TaskDetailsForm(instance=user_task)
+    if request.method == 'POST':
+        form = TaskDetailsForm(request.POST, instance=user_task)
+        if form.is_valid():
+            form.save()
+            return redirect('inbox')
+    context = {'form':form, 'slug':slug, 'no_of_undone_tasks':lst_undone_task[-1]}
+    return render(request, 'update_task.html', context)
 
 @login_required(login_url='login')
 def TaskDetail(request, slug):
