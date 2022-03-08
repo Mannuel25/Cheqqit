2fc42ffd4146320ba2426e5c8158674b7a8a9afc
mannuel25 <oluwasegunprosperity@gmail.com>
2022-03-08 14:55:19 +0100

    update

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'cheqqit.CustomUser'
 
