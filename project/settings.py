0ebea9fe3baaef58f64cfa1512f04a7deb928c01
mannuel25 <oluwasegunprosperity@gmail.com>
2022-03-08 15:21:58 +0100

    fix

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['cheqqit.herokuapp.com']
 
 AUTH_USER_MODEL = 'cheqqit.CustomUser'
 
