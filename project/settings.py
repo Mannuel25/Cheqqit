82152048feb43bc63fb84f9c7894b157a127a89a
mannuel25 <oluwasegunprosperity@gmail.com>
2022-03-08 22:12:50 +0100

    update

--- project/settings.py
+++ project/settings.py
@@ -121,7 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-# STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
