6d104821516e68ca6225e33a675e6794e38dfedf
mannuel25 <oluwasegunprosperity@gmail.com>
2022-04-05 17:31:04 +0100

    update forms..

--- pages/forms.py
+++ pages/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import UserTasks
 
-class TaskDetailsForm(forms.ModelForm):
+class AddTaskForm(forms.ModelForm):
     """
     this form will be used to either create a task 
     or update the task created
@@ -23,7 +23,31 @@ class TaskDetailsForm(forms.ModelForm):
         model = UserTasks
         fields = ('title', 'description','task_due_date','task_due_time',)
 
-class ViewTaskDetailsForm(forms.ModelForm):
+class UpdateTaskForm(forms.ModelForm):
+    """
+    this form will be used to update the task created
+    """
+    title = forms.CharField(max_length=100, widget= forms.TextInput
+        (attrs={'placeholder':"Enter the task's title"}))
+    description = forms.CharField(max_length=150, required=False, widget= forms.Textarea
+        (attrs={'placeholder':"Enter the task's description"}))
+    task_due_date = forms.DateField(
+        label="Select the task's due date", required=False,
+        widget=forms.widgets.DateInput(attrs={'type':'date'})
+    )
+    task_due_time = forms.TimeField(
+        label="Select the task's due time", required=False,
+        widget=forms.widgets.TimeInput(attrs={'type':'time'})
+    )
+    completed_task = forms.BooleanField(label='Completed this task?',
+        required = False, widget=forms.widgets.CheckboxInput(attrs={'class': 'checkbox-inline'}),
+    )
+    
+    class Meta(forms.ModelForm):
+        model = UserTasks
+        fields = ('title', 'description','task_due_date','task_due_time', 'completed_task',)
+
+class TaskDetailsForm(forms.ModelForm):
     """
     this form will be used to only view the task/goal created
     """
@@ -39,12 +63,10 @@ class ViewTaskDetailsForm(forms.ModelForm):
         label="Task's due time", required=False,
         widget=forms.widgets.TimeInput(attrs={'type':'time','readonly':'readonly'})
     )
-    completed_task = forms.BooleanField(label='Completed this task?',
-        required = False, widget=forms.widgets.CheckboxInput(attrs={'class': 'checkbox-inline'}),
-    )
+    
     class Meta(forms.ModelForm):
         model = UserTasks
-        fields = ('title', 'description','task_due_date','task_due_time','completed_task',)
+        fields = ('title', 'description','task_due_date','task_due_time',)
 
 
 class AllTasksForm(forms.ModelForm):
