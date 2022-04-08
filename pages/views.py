b077e0834cc41f847d3d8124c8038af5e55c39cf
mannuel25 <oluwasegunprosperity@gmail.com>
2022-04-08 15:38:12 +0100

    ...

--- pages/views.py
+++ pages/views.py
@@ -37,7 +37,7 @@ class InboxView(LoginRequiredMixin, ListView):
         context['no_of_undone_tasks'] = context['tasks'].filter(completed_task=False).count()
         number_of_undone_tasks.append(context['no_of_undone_tasks'])
         context['all_completed_tasks'] = set(all_completed_tasks) 
-        
+
         if True in task_completed:
             selected_task = ' '.join(i for i in get_task_title)
             if len(selected_task) > 8:
@@ -46,6 +46,7 @@ class InboxView(LoginRequiredMixin, ListView):
                 selected_task = selected_task
             messages.success(self.request, f'{selected_task} successfully completed!')    
             get_task_title.clear()
+            task_completed.clear()
 
         search_input = self.request.GET.get('search-area') or ''
         if search_input:
