796b4a510424426908270c5d8c8464abdd476459
mannuel25 <oluwasegunprosperity@gmail.com>
2022-04-15 22:39:30 +0100

    update signup form

--- cheqqit/forms.py
+++ cheqqit/forms.py
@@ -3,6 +3,16 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
+    password1 = forms.CharField(
+        label= ("Password"),
+        strip=False,
+        widget=forms.PasswordInput,
+    )
+    password2 = forms.CharField(
+        label= ("Password confirmation"),
+        widget=forms.PasswordInput,
+        strip=False,
+    )
     class Meta(UserCreationForm):
         model = CustomUser
         fields = ('username', 'email')
