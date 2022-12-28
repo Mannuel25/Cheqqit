7033a284ead2fe39307c60f4267b738ff7380e78
mannuel25 <oluwasegunprosperity@gmail.com>
2022-12-28 16:30:54 +0100

    configure staticfiles

--- project/settings.py
+++ project/settings.py
@@ -127,9 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-# Extra places for collectstatic to find static files.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
